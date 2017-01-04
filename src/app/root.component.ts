278dd4e47b4556a05dd66a05925c7810c7bb22f3
Michael Stuart <mistuart@corelogic.com>
2017-01-04 11:55:45 -0600

    Use relative routes for links in /
    Signed-off-by: Grant Hutchins <ghutchins@pivotal.io>

--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -3,8 +3,8 @@ import {Component} from "@angular/core";
 @Component({
   template: `
 <p>This is the root component.</p>
-<p><a href="/redirect-to-root">Go to /redirect-to-root (full page refresh)</a></p>
-<p><a href="/redirect-to-elsewhere">Go to /redirect-to-elsewhere (full page refresh)</a></p>
+<p><a href="./redirect-to-root">Go to /redirect-to-root (full page refresh)</a></p>
+<p><a href="./redirect-to-elsewhere">Go to /redirect-to-elsewhere (full page refresh)</a></p>
 `
 })
 export class RootComponent {
